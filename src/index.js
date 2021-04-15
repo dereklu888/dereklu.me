7d73101c12abd0ce80e7f5820bb2386908ea3362
Derek Lu <derek.s.lu.23@dartmouth.edu>
2021-04-15 17:11:55 -0500

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("starting up!");
+import $ from 'jquery';
+import './style.scss';
+
+let time = 0;
+setInterval(() => {
+  time += 1;
+  $('#main').html(`You've been on this page for ${time} seconds.`);
+}, 1000);
+
+$('#main').html('Here we go!');
